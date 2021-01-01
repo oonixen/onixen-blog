848cf0dade7e94f66496286625ced1b52c26b824
NikitaSorokinn <sorokinn1398@gmail.com>
2021-01-01 23:28:49 +0400

    Develop portfolio

---
+++ src/js/modules/portfolio.js
@@ -0,0 +1,76 @@
+const linkClass = 'Portfolio__link'
+const linkClassActive = 'Portfolio__link--active'
+const portfolioSectionClass = 'Portfolio__project'
+const linkArray = document.getElementsByClassName(linkClass);
+const portfolioSectionArray = Array.from(document.getElementsByClassName(portfolioSectionClass)).map(v => {
+
+    const elemRect = v.getBoundingClientRect()
+
+    return {
+        top: elemRect.top + window.pageYOffset,
+        bottom: elemRect.bottom + window.pageYOffset
+    }
+})
+
+for (let item of linkArray) item.addEventListener('click', clickOnLink)
+document.addEventListener('scroll', onScroll)
+
+function clickOnLink (e) {
+
+    const innerText = e.target.innerText
+    let index = 0
+
+    for (let i = 0; i < linkArray.length; i++) {
+        if (linkArray[i].innerText === innerText) {
+            index = i
+            break
+        }
+    }
+
+    addActiveClass(index)
+}
+
+function onScroll () {
+
+    const windowTopHeight = window.pageYOffset + 100
+
+    for (let i = 0; i < portfolioSectionArray.length; i++) {
+
+        const elemY1 = portfolioSectionArray[i]
+        const elemY2 = portfolioSectionArray[i+1]
+
+        if (elemY2 === undefined) {
+            if (windowTopHeight >= elemY1.top && windowTopHeight <= elemY1.bottom) {
+                addActiveClass(i)
+                break
+            } else {
+                break
+            }
+        }
+        else if (windowTopHeight >= elemY1.top && windowTopHeight <= elemY2.top) {
+            addActiveClass(i)
+            break
+        } else {
+            clearActiveClass()
+        }
+    }
+}
+
+function addActiveClass (i) {
+
+    const classList = linkArray[i].classList
+
+    if (!classList.contains(linkClassActive)) {
+        clearActiveClass()
+        classList.add(linkClassActive)
+    }
+}
+
+function clearActiveClass() {
+    for (let item of linkArray) {
+        if (item.classList.contains(linkClassActive)) {
+            item.classList.remove(linkClassActive)
+            break
+        }
+    }
+}
